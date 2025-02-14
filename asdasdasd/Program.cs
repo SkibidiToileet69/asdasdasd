d06ad4a6647b8b299247aac4e4290cecbb776331
MEGA <MEGA@gmail.com>
2025-02-14 16:27:32 +0200

    Add project files.

---
+++ asdasdasd/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace asdasdasd
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());  
+            if (IsPowerOfTwo(n))
+            {
+                Console.WriteLine($"{n} е степен на 2.");
+            }
+            else
+            {
+                Console.WriteLine($"{n} не е степен на 2.");
+            }
+        }
+
+        static bool IsPowerOfTwo(int n)
+        {
+ 
+            if (n <= 0)
+                return false;
+
+            if (n == 1)
+                return true;
+
+            if (n % 2 != 0)
+                return false;
+
+            return IsPowerOfTwo(n / 2);
+        }
+    }
+}
