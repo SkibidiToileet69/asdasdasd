81985dc9e08b546cb6d32c18f4b792528275cb49
MEGA <MEGA@gmail.com>
2025-02-14 17:23:27 +0200

    zad4

---
+++ zad4rekurenciq/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace zad4rekurenciq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int pow = int.Parse(Console.ReadLine());
+            Console.WriteLine(Power(num, pow));
+        }
+        static int Power(int num, int pow) 
+        {
+            if (pow == 0)
+            {
+                return 1;
+            }
+            return num * Power(num, pow - 1);
+        }
+    }
+}
