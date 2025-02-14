a2503ed62b7ee1e31630fb92e0585a89c1a20944
MEGA <MEGA@gmail.com>
2025-02-14 16:56:34 +0200

    Yes

---
+++ zad3rekurenciq/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace zad3rekurenciq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Reverse(n));
+        }
+        static int Reverse(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            return (n % 10) * (int)Math.Pow(10, (int)Math.Log10(n)) + Reverse(n/ 10);
+        } 
+    }
+}
